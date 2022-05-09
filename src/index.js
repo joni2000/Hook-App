7715aac03e369bd8bfcd432b0d170000265972ee
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-05-09 19:53:21 -0300

    cree un custom hook llamado useFetch para realizar peticiones

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook'
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 
 /* import SimpleForm from './components/02-useEffect/SimpleForm'; */
 /* import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook'; */
 /* import CounterApp from './components/01-useState/CounterApp'; */
+/* import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook' */
 //import HookApp from './HookApp';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
 );
 
