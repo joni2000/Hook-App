37407e0d58b85a3acd1445a8524f78941399a091
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-05-13 21:37:35 -0300

    creando la base de nuestra app de tareas

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { TodoApp } from './components/08-useReducer/TodoApp';
+
 // import { Padre } from './components/07-tarea-memo/Padre';
+// import { CallbackHook } from './components/06-memos/CallbackHook'; 
+// import { MemoHook } from './components/06-memos/MemoHook'; 
+// import { Layout } from './components/05-useLayoutEffect/Layout';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef'; 
+// import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook'; 
+// import CounterApp from './components/01-useState/CounterApp'; 
+// import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook' 
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'; 
+// import HookApp from './HookApp';
+// import { FocusScreen } from './components/04-useRef/FocusScreen'; 
+// import { Memorize } from './components/06-memos/Memorize';
 
-// /* import { CallbackHook } from './components/06-memos/CallbackHook'; */
-// /* import { MemoHook } from './components/06-memos/MemoHook'; */
-// //import { Layout } from './components/05-useLayoutEffect/Layout';
-// /* import { RealExampleRef } from './components/04-useRef/RealExampleRef'; */
-// /* import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook'; */
-// /* import CounterApp from './components/01-useState/CounterApp'; */
-// /* import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook' */
-// /* import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'; */
-// //import HookApp from './HookApp';
-// /* import { FocusScreen } from './components/04-useRef/FocusScreen'; */
-// /* import { Memorize } from './components/06-memos/Memorize'; */
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <TodoApp />
+);
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <Padre />
-// );
 
-import './components/08-useReducer/intro-reducer'
